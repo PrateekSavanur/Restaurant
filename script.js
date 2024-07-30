7188532c0dff4f5a4df413fd3bb4307bf8854235
PrateekSavanur <prateek.savanur@gmail.com>
2024-07-30 19:37:32 +0530

    Design reconfigure

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => callClass());
+document.addEventListener("DOMContentLoaded", () => {
+  callClass();
+  document.getElementById("current-year").textContent =
+    new Date().getFullYear();
+});
 
 const callClass = () => {
   const galleryButtons = document.querySelectorAll(".gallery-btn");
   const galleryDisplay = document.querySelector(".gallery-display");
 
   const galleryImages = {
-    1: ["./assets/1.jpeg", "./assets/1.jpeg", "./assets/1.jpeg"],
-    2: ["./assets/2.jpeg", "./assets/1.jpeg", "./assets/2.jpeg"],
+    1: ["./assets/1.jpeg", "./assets/2.jpeg", "./assets/2.jpeg"],
+    2: ["./assets/1.jpeg", "./assets/1.jpeg", "./assets/1.jpeg"],
     3: ["./assets/1.jpeg", "./assets/1.jpeg", "./assets/1.jpeg"],
-    4: ["./assets/2.jpeg", "./assets/2.jpeg", "./assets/2.jpeg"],
+    4: ["./assets/1.jpeg", "./assets/1.jpeg", "./assets/1.jpeg"],
+  };
+
+  const galleryTitles = {
+    1: "Gallery 1",
+    2: "Gallery 2",
+    3: "Gallery 3",
+    4: "Gallery 4",
   };
 
   galleryButtons.forEach((button) => {
@@ -18,10 +29,18 @@ const callClass = () => {
     });
   });
 
+  displayGalleryImages(Object.keys(galleryImages)[0]);
+
   function displayGalleryImages(galleryId) {
     galleryDisplay.innerHTML = "";
+
+    const heading = document.createElement("h2");
+    heading.textContent = galleryTitles[galleryId];
+    galleryDisplay.appendChild(heading);
+
     const marquee = document.createElement("div");
     marquee.className = "marquee";
+    heading.className = "text-center mb-3";
 
     galleryImages[galleryId].forEach((imageSrc) => {
       const img = document.createElement("img");
@@ -34,5 +53,3 @@ const callClass = () => {
     galleryDisplay.style.display = "block";
   }
 };
-
-document.getElementById("current-year").textContent = new Date().getFullYear();
